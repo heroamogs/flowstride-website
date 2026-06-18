8113fa318c06b69e9bafe7ac9b9cdf6b0a2136b2
Samuel Okolo <herookolo@gmail.com>
2026-06-18 22:41:02 +0100

    update: Removed price from landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import Features from "@/components/Features";
 import UnifiedTesting from "@/components/UnifiedTesting";
 import CallToAction from "@/components/CallToAction";
 import Footer from "@/components/Footer";
-import Pricing from "@/components/Pricing";
 
 export default function Home() {
   return (
@@ -13,7 +12,6 @@ export default function Home() {
       <Comparison />
       <Features />
       <UnifiedTesting />
-      <Pricing />
       <CallToAction />
       <Footer />
     </main>
